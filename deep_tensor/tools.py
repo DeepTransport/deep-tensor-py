aed4b2ebe68db690d04c011c4bbe95a4be307949
alexgdebeer <alexgdebeer@gmail.com>
2024-11-22 14:07:06 +1100

    Tidy up tools.py

--- deep_tensor/tools.py
+++ deep_tensor/tools.py
@@ -19,16 +19,14 @@ def compute_ess_ratio(log_weights: torch.Tensor) -> torch.Tensor:
 
     References
     ----------
-    Owen, AB (2013). Monte Carlo theory, methods and examples. Chapter 
-    9.
+    Owen, AB (2013). Monte Carlo theory, methods and examples. Chapter 9.
 
     """
 
     sample_size = log_weights.numel()
     log_weights = log_weights - log_weights.max()
 
-    ess = (log_weights.exp().sum().square() 
-           / (2.0*log_weights).exp().sum())
+    ess = log_weights.exp().sum().square() / (2.0*log_weights).exp().sum()
     ess_ratio = ess / sample_size
     return ess_ratio
 
