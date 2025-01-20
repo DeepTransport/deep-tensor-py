3d8bd31f3ce5be540b994fd8321088321c1dd35b
alexgdebeer <alexgdebeer@gmail.com>
2025-01-20 13:37:45 +1100

    Add derivative tests for Lagrange1D polynomial

--- tests/test_polynomials/test_piecewise.py
+++ tests/test_polynomials/test_piecewise.py
@@ -57,3 +57,34 @@ class TestSpectralPolynomials(unittest.TestCase):
         self.assertTrue((func_vals_true - func_vals).abs().max() < 1e-8)
 
         return
+    
+    def test_lagrange_1d_deriv(self):
+        """Verifies that the derivative operations of a Lagrange1 
+        polynomial work as intended.
+        """
+
+        poly = dt.Lagrange1(num_elems=4)
+
+        ls = torch.linspace(-0.75, 0.75, 4)
+        coeffs = torch.tensor([[2.0], [3.0], [2.0], [3.0], [2.0]])
+
+        deriv_vals = poly.eval_basis_deriv(ls)
+        weights_deriv = poly.eval_measure_deriv(ls)
+        radon_deriv_vals = poly.eval_radon_deriv(coeffs, ls)
+        func_deriv_vals = poly.eval_deriv(coeffs, ls)
+
+        deriv_vals_true = torch.tensor([
+            [-2.0, 2.0, 0.0, 0.0, 0.0],
+            [0.0, -2.0, 2.0, 0.0, 0.0],
+            [0.0, 0.0, -2.0, 2.0, 0.0],
+            [0.0, 0.0, 0.0, -2.0, 2.0]
+        ])
+
+        weights_deriv_true = torch.zeros((4,))
+        radon_deriv_vals_true = torch.tensor([[2.0], [-2.0], [2.0], [-2.0]])
+        func_deriv_vals_true = torch.tensor([[1.0], [-1.0], [1.0], [-1.0]])
+
+        self.assertTrue((deriv_vals_true - deriv_vals).abs().max() < 1e-8)
+        self.assertTrue((weights_deriv_true - weights_deriv).abs().max() < 1e-8)
+        self.assertTrue((radon_deriv_vals_true - radon_deriv_vals).abs().max() < 1e-8)
+        self.assertTrue((func_deriv_vals_true - func_deriv_vals).abs().max() < 1e-8)
