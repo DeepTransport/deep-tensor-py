d5b0eb85101224977b1af06209f30ac0f1bceede
alexgdebeer <alexgdebeer@gmail.com>
2025-01-20 11:35:12 +1100

    Add new polynomial tests

---
+++ tests/test_polynomials_piecewise.py
@@ -0,0 +1,60 @@
+import unittest
+
+import torch
+
+import deep_tensor as dt
+
+
+torch.manual_seed(0)
+
+
+class TestSpectralPolynomials(unittest.TestCase):
+
+    def test_lagrange_1d(self):
+        """Confirms that the product of the basis2node and node2basis 
+        matrices is the identity matrix (see Cui and Dolgov 2022, 
+        Appendix A).
+        """
+
+        poly = dt.Lagrange1(num_elems=4)
+
+        ls = torch.linspace(-1.0, 1.0, 9)
+        coeffs = torch.tensor([[2.0], [3.0], [2.0], [3.0], [2.0]])
+
+        basis_vals = poly.eval_basis(ls)
+        weights = poly.eval_measure(ls)
+        radon_vals = poly.eval_radon(coeffs, ls)
+        func_vals = poly.eval(coeffs, ls)
+
+        basis_vals_true = torch.tensor([
+            [1.0, 0.0, 0.0, 0.0, 0.0],
+            [0.5, 0.5, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0, 0.0],
+            [0.0, 0.5, 0.5, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 0.5, 0.5, 0.0],
+            [0.0, 0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 0.5, 0.5],
+            [0.0, 0.0, 0.0, 0.0, 1.0]
+        ])
+
+        weights_true = torch.full((9,), 0.5)
+
+        radon_vals_true = torch.tensor([
+            [2.0], [2.5], [3.0], 
+            [2.5], [2.0], [2.5], 
+            [3.0], [2.5], [2.0]
+        ])
+
+        func_vals_true = torch.tensor([
+            [1.00], [1.25], [1.50],
+            [1.25], [1.00], [1.25],
+            [1.50], [1.25], [1.00]
+        ])
+
+        self.assertTrue((basis_vals_true - basis_vals).abs().max() < 1e-8)
+        self.assertTrue((weights_true - weights).abs().max() < 1e-8)
+        self.assertTrue((radon_vals_true - radon_vals).abs().max() < 1e-8)
+        self.assertTrue((func_vals_true - func_vals).abs().max() < 1e-8)
+
+        return
