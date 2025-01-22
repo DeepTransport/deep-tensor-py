5201b431416557e00b78ecd9303ca17bac6e7203
alexgdebeer <alexgdebeer@gmail.com>
2025-01-23 10:48:16 +1100

    Add test for build_block_local

--- tests/test_tt_func.py
+++ tests/test_tt_func.py
@@ -101,7 +101,49 @@ class TestSIRT(unittest.TestCase):
 
         self.assertTrue(norm(ps_forward - ps_true) < 1e-8)
         self.assertTrue(norm(ps_backward - ps_true) < 1e-8)
-        return 
+        return
+
+    def test_build_block_local(self):
+        """Verifies that build_block_local is working as intended.
+        """
+
+        def target_func(ls: torch.Tensor):
+            return ls.sum(dim=1)
+        
+        poly = dt.Lagrange1(num_elems=2)
+        domain = dt.BoundedDomain()
+        dim = 3
+        bases = dt.ApproxBases(poly, domain, dim)
+
+        tt_func = dt.TTFunc(
+            target_func, 
+            bases, 
+            options=dt.TTOptions(init_rank=3),
+            input_data=dt.InputData()
+        )
+
+        ls_left = torch.tensor([[0.5],
+                                [-0.5],
+                                [1.0]])
+
+        ls_right = torch.tensor([[0.0],
+                                 [0.5],
+                                 [1.0]])
+
+        F_k = tt_func.build_block_local(ls_left, ls_right, 1)
+
+        F_k_true = torch.tensor([[[-0.5,  0.0,  0.5],
+                                  [ 0.5,  1.0,  1.5],
+                                  [ 1.5,  2.0,  2.5]],
+                                 [[-1.5, -1.0, -0.5],
+                                  [-0.5,  0.0,  0.5],
+                                  [ 0.5,  1.0,  1.5]],
+                                 [[ 0.0,  0.5,  1.0],
+                                  [ 1.0,  1.5,  2.0],
+                                  [ 2.0,  2.5,  3.0]]])
+
+        self.assertTrue(norm(F_k - F_k_true) < 1e-8)
+        return
 
 
 if __name__ == "__main__":
