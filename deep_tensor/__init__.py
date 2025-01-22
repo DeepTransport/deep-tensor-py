909ba19acf69e11ae65ada4b26014dba885bf4e5
alexgdebeer <alexgdebeer@gmail.com>
2025-01-22 20:12:30 +1100

    Export TTFunc

--- deep_tensor/__init__.py
+++ deep_tensor/__init__.py
@@ -1,5 +1,3 @@
-# TODO: finish this
-
 import torch
 torch.set_default_dtype(torch.float64)
 
@@ -7,6 +5,7 @@ from .approx_bases import ApproxBases
 from .bridging_densities import Tempering1
 from .directions import Direction
 from .domains import BoundedDomain
+from .irt import TTDIRT, TTSIRT
 from .input_data import InputData
 from .options import TTOptions
 from .polynomials import (
@@ -22,4 +21,4 @@ from .polynomials import (
     PiecewiseCDF,
     Spectral
 )
-from .irt import TTDIRT, TTSIRT
+from .tt_func import TTFunc
