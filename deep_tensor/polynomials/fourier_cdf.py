50d489154a7a996f4b02244744d15d1296321b72
alexgdebeer <alexgdebeer@gmail.com>
2025-02-11 11:32:36 +1100

    Update Fourier CDF

--- deep_tensor/polynomials/fourier_cdf.py
+++ deep_tensor/polynomials/fourier_cdf.py
@@ -1,6 +1,7 @@
 from typing import Tuple
 
 import torch
+from torch import Tensor
 
 from .spectral_cdf import SpectralCDF
 from .fourier import Fourier
@@ -15,32 +16,42 @@ class FourierCDF(SpectralCDF, Fourier):
         return
     
     @property
-    def domain(self) -> torch.Tensor:
+    def domain(self) -> Tensor:
         return self._domain 
     
     @property
-    def node2basis(self) -> torch.Tensor:
+    def node2basis(self) -> Tensor:
         return self._node2basis
     
-    @property
-    def basis2node(self) -> torch.Tensor:
-        return self._basis2node
+    @node2basis.setter
+    def node2basis(self, value: Tensor) -> None:
+        self._node2basis = value 
+        return None
     
     @property
-    def nodes(self) -> torch.Tensor:
+    def basis2node(self) -> Tensor:
+        return self._basis2node
+    
+    @basis2node.setter
+    def basis2node(self, value: Tensor) -> None:
+        self._basis2node = value 
+        return None
+    
+    @property
+    def nodes(self) -> Tensor:
         return self._nodes
 
     @property 
     def cardinality(self) -> int:
         return self.nodes.numel()
 
-    def grid_measure(self, n: int) -> torch.Tensor:
+    def grid_measure(self, n: int) -> Tensor:
         us = torch.linspace(*self.domain, n)
         us[0] = self.domain[0] - EPS 
         us[-1] = self.domain[-1] + EPS
         return us
 
-    def eval_int_basis(self, us: torch.Tensor) -> torch.Tensor:
+    def eval_int_basis(self, us: Tensor) -> Tensor:
 
         tmp = torch.outer(us, self.c)
         
@@ -54,11 +65,7 @@ class FourierCDF(SpectralCDF, Fourier):
         
         return int_basis_vals
     
-    def eval_int_basis_newton(
-        self, 
-        us: torch.Tensor
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        
+    def eval_int_basis_newton(self, us: Tensor) -> Tuple[Tensor, Tensor]:
         basis_vals = self.eval_int_basis(us)
         deriv_vals = self.eval_basis(us)
         return basis_vals, deriv_vals
