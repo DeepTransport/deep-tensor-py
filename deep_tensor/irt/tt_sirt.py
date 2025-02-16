Fix Jacobian implementation

deep_tensor/irt/tt_sirt.py
@@ -603,9 +603,11 @@ class TTSIRT(AbstractIRT):
         # Populate off-diagonal elements
         for k in range(1, self.dim):
             for j in range(k):
-                grad_cond = (ps_grid_deriv[k][j] * ps_marg[k-1] - ps_grid[k] * ps_marg_deriv[k-1][j]) / ps_marg[k-1].square() * wls[k]
+                grad_cond = (ps_grid_deriv[k][j] * ps_marg[k-1] - ps_grid[k] * ps_marg_deriv[k-1][j]) / ps_marg[k-1].square()
+                if polys[k].constant_weight:
+                    grad_cond *= wls[k]
                 Jacs[k, :, j] = self.oned_cdfs[k].eval_int_deriv(grad_cond, ls[:, k])
-            
+
         return Jacs
     
     def _eval_rt_jac_local_backward(self, ls: Tensor) -> Tensor:
@@ -659,10 +661,10 @@ class TTSIRT(AbstractIRT):
             ps_grid[k] = gs_grid.square().sum(dim=(2, 3)) + self.tau
 
         # Derivatives of marginal PDF
-        for k in range(1, self.dim):  # rows
+        for k in range(1, self.dim):
             ps_marg_deriv[k] = {}
 
-            for j in range(k, self.dim):  # cols
+            for j in range(k, self.dim):
 
                 prod = TTFunc.batch_mul(Gs_prod[k+1], Ps[k])
                 prod_deriv = torch.ones((n_ls, 1, 1))
@@ -697,7 +699,9 @@ class TTSIRT(AbstractIRT):
         # Populate off-diagonal elements
         for k in range(self.dim-1):
             for j in range(k+1, self.dim):
-                grad_cond = (ps_grid_deriv[k][j] * ps_marg[k+1] - ps_grid[k] * ps_marg_deriv[k+1][j]) / ps_marg[k+1].square() * wls[k]
+                grad_cond = (ps_grid_deriv[k][j] * ps_marg[k+1] - ps_grid[k] * ps_marg_deriv[k+1][j]) / ps_marg[k+1].square()
+                if polys[k].constant_weight:
+                    grad_cond *= wls[k]
                 Jacs[k, :, j] = self.oned_cdfs[k].eval_int_deriv(grad_cond, ls[:, k])
             
         return Jacs

deep_tensor/polynomials/cdfs/chebyshev_2nd_trigo_cdf.py
@@ -52,7 +52,7 @@ class Chebyshev2ndTrigoCDF(TrigoCDF, Chebyshev2nd):
         thetas = thetas[:, None]
 
         cdf_ind = torch.arange(1, self.order+3)
-        temp = torch.sin(cdf_ind * thetas) / cdf_ind 
+        temp = torch.sin(thetas * cdf_ind) / cdf_ind 
         ps = torch.hstack((
             thetas - temp[:, 1][:, None],
             temp[:, :self.order] - temp[:, 2:]

deep_tensor/polynomials/polynomials/chebyshev_2nd.py
@@ -94,7 +94,7 @@ class Chebyshev2nd(Spectral):
 
         # Deal with endpoints
         mask_lhs = (ls + 1.0).abs() < EPS
-        mask_rhs = (ls - 1.0).abs() < EPS 
+        mask_rhs = (ls - 1.0).abs() < EPS
         ps[mask_lhs] = self.norm * (self.n+1) * torch.tensor(-1.0).pow(self.n)
         ps[mask_rhs] = self.norm * (self.n+1)
         check_finite(ps)

deep_tensor/polynomials/polynomials/chebyshev_2nd_unweighted.py
@@ -1,4 +1,6 @@
 import torch 
+from torch import Tensor
+from torch.distributions.beta import Beta
 
 from .spectral import Spectral
 from ...constants import EPS
@@ -24,65 +26,61 @@ class Chebyshev2ndUnweighted(Spectral):
         return
     
     @property
-    def domain(self) -> torch.Tensor:
+    def domain(self) -> Tensor:
         return self._domain
 
     @property
-    def nodes(self) -> torch.Tensor:
+    def nodes(self) -> Tensor:
         return self._nodes 
     
     @nodes.setter
-    def nodes(self, value: torch.Tensor) -> None: 
+    def nodes(self, value: Tensor) -> None: 
         self._nodes = value 
         return
     
     @property
-    def weights(self) -> torch.Tensor:
+    def weights(self) -> Tensor:
         return self._weights
     
     @property
     def constant_weight(self) -> bool:
         return self._constant_weight
 
-    def sample_measure(self, n: int) -> torch.Tensor:
+    def sample_measure(self, n: int) -> Tensor:
         
-        beta = torch.distributions.beta.Beta(1.5, 1.5)
+        beta = Beta(1.5, 1.5)
         rs = beta.sample((n, self.order))
         rs = 2.0 * rs - 1.0
         return rs
     
-    def sample_measure_skip(self, n: int) -> torch.Tensor:
-
+    def sample_measure_skip(self, n: int) -> Tensor:
         r0 = 0.5 * (torch.min(self.nodes) - 1.0)
         r1 = 0.5 * (torch.max(self.nodes) + 1.0)
-        
         rs = torch.rand(n) * (r1-r0) + r0
         return rs
     
-    def eval_measure(self, rs: torch.Tensor) -> torch.Tensor:
-        
-        ts = 1.0 - rs **2
+    def eval_measure(self, ls: Tensor) -> Tensor:
+        ts = 1.0 - ls.square()
         ts[ts < EPS] = 0.0
         ws = 2.0 * torch.sqrt(ts) / torch.pi 
         return ws
     
-    def eval_log_measure(self, rs: torch.Tensor) -> torch.Tensor:
-        
-        ts = 1.0 - rs **2
+    def eval_log_measure(self, rs: Tensor) -> Tensor:
+        ts = 1.0 - rs.square()
         ts[ts < EPS] = 0.0
-        ws = 0.5*torch.log(ts) + torch.log(torch.tensor(2.0)/torch.pi)
+        ws = 0.5*torch.log(ts) + (torch.tensor(2.0) / torch.pi).log()
         return ws
     
-    def eval_measure_deriv(self, rs: torch.Tensor) -> torch.Tensor:
-        ts = 1.0 - rs **2
+    def eval_measure_deriv(self, ls: Tensor) -> Tensor:
+        ts = 1.0 - ls.square()
         ts[ts < EPS] = 0.0
-        ws = -rs * torch.sqrt(ts) * 2.0 / torch.pi
+        ws = -ls * ts.sqrt() * 2.0 / torch.pi
         return ws
     
-    def eval_log_measure_deriv(self, rs: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError("Not implemented.")
+    def eval_log_measure_deriv(self, ls: Tensor) -> Tensor:
+        raise NotImplementedError()
 
-    def eval_basis(self, ls: torch.Tensor) -> torch.Tensor:
+    def eval_basis(self, ls: Tensor) -> Tensor:
 
         thetas = self.l2theta(ls)
         fs = (torch.sin(torch.outer(self.n+1, thetas)) 
@@ -96,11 +94,5 @@ class Chebyshev2ndUnweighted(Spectral):
         
         return fs
 
-    def eval_basis_deriv(self, rs: torch.Tensor) -> torch.Tensor:
-        
-        theta = self.l2theta(rs)
-            
-        fs = (torch.cos(theta*(self.n+1)) * (self.n+1) - torch.sin(theta * (self.n+1)) * (rs / torch.sin(theta)) ) / (rs**2 - 1)
-        fs = fs * self.normalising
-        
-        return fs
+    def eval_basis_deriv(self, ls: Tensor) -> Tensor:
+        raise NotImplementedError()

deep_tensor/polynomials/polynomials/legendre.py
@@ -8,9 +8,6 @@ class Legendre(Recurr):
 
     def __init__(self, order: int):
 
-        self.domain = torch.tensor([-1.0, 1.0])
-        self.constant_weight = True
-
         n = torch.arange(order+1)
         a = (2*n + 1) / (n + 1)
         b = torch.zeros(n.shape)
@@ -22,21 +19,11 @@ class Legendre(Recurr):
 
     @property
     def domain(self) -> Tensor:
-        return self._domain
-    
-    @domain.setter
-    def domain(self, value: Tensor) -> None:
-        self._domain = value
-        return
+        return torch.tensor([-1.0, 1.0])
     
     @property
     def constant_weight(self) -> bool:
-        return self._constant_weight
-    
-    @constant_weight.setter
-    def constant_weight(self, value: Tensor) -> None:
-        self._constant_weight = value
-        return
+        return True
     
     @property 
     def nodes(self) -> Tensor: