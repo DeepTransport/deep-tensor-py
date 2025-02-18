193e01c34d0c35541274d822dedcf9a8373bc6e3
alexgdebeer <adeb970@aucklanduni.ac.nz>
2025-02-18 20:56:48 +1300

    Change options classes to dataclasses

--- deep_tensor/options/dirt_options.py
+++ deep_tensor/options/dirt_options.py
@@ -1,26 +1,21 @@
+from dataclasses import dataclass
+
 from .verification import verify_method
 
 
 METHODS = ["eratio", "aratio"]
 
 
+@dataclass
 class DIRTOptions():
 
-    def __init__(
-        self, 
-        method: str="aratio",
-        max_layers: int=50,
-        num_samples: int=1000,
-        num_debugs: int=1000,
-        defensive: float=1e-8
-    ):
+    method: str = "aratio"
+    max_layers: int = 50
+    num_samples: int = 1000
+    num_debugs: int = 1000
+    defensive: float = 1e-8
         
-        method = method.lower()
-        verify_method(method, METHODS)
-
-        self.method = method
-        self.max_layers = max_layers
-        self.num_samples = num_samples
-        self.num_debugs = num_debugs 
-        self.defensive = defensive 
+    def __post_init__(self):
+        self.method = self.method.lower()
+        verify_method(self.method, METHODS)
         return
--- deep_tensor/options/tt_options.py
+++ deep_tensor/options/tt_options.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from .verification import verify_method
 
 
@@ -5,35 +7,25 @@ TT_METHODS = ["random", "amen", "fixed_rank"]
 INT_METHODS = ["qdeim", "deim", "maxvol"]
 
 
+@dataclass
 class TTOptions():
-
-    def __init__(
-        self, 
-        max_als: int=4,
-        als_tol: float=1e-4,
-        init_rank: int=20,
-        kick_rank: int=2,
-        max_rank: int=30, 
-        local_tol: float=1e-10,
-        cdf_tol: float=1e-10,
-        tt_method: str="amen",
-        int_method: str="maxvol",
-        verbose: bool=True
-    ):
-
-        tt_method = tt_method.lower()
-        int_method = int_method.lower()
-        verify_method(tt_method, TT_METHODS)
-        verify_method(int_method, INT_METHODS)
         
-        self.max_als = max_als
-        self.als_tol = als_tol
-        self.init_rank = init_rank
-        self.kick_rank = kick_rank
-        self.max_rank = max_rank
-        self.local_tol = local_tol
-        self.cdf_tol = cdf_tol
-        self.tt_method = "fixed_rank" if kick_rank == 0 else tt_method
-        self.int_method = int_method
-        self.verbose = verbose
+    max_als: int = 4
+    als_tol: float = 1e-4
+    init_rank: int = 20
+    kick_rank: int = 2
+    max_rank: int = 30
+    local_tol: float = 1e-10
+    cdf_tol: float = 1e-10
+    tt_method: str = "amen"
+    int_method: str = "maxvol"
+    verbose: bool = True
+    
+    def __post_init__(self):
+        if self.kick_rank == 0:
+            self.tt_method = "fixed_rank"
+        self.tt_method = self.tt_method.lower()
+        self.int_method = self.int_method.lower()
+        verify_method(self.tt_method, TT_METHODS)
+        verify_method(self.int_method, INT_METHODS)
         return
