99a564a026141f8f4a1f7b90b17b8d3575355392
alexgdebeer <adeb970@aucklanduni.ac.nz>
2025-02-18 21:00:45 +1300

    Rename tools.validation

--- deep_tensor/tools/__init__.py
+++ deep_tensor/tools/__init__.py
@@ -2,4 +2,4 @@ from .ess import compute_ess_ratio
 from .f_divergences import compute_f_divergence
 from .interpolation import deim, lu_deim, maxvol
 from .integration import integrate
-from .validation import check_finite
+from .verification import check_finite
