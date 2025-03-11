51b86fd6e3ed5c79a95e16f296e0369cc227dffa
alexgdebeer <alexgdebeer@gmail.com>
2025-03-11 12:52:49 +1100

    Add documentation for Fourier polynomials

--- deep_tensor/polynomials/spectral/fourier.py
+++ deep_tensor/polynomials/spectral/fourier.py
@@ -5,12 +5,62 @@ from .spectral import Spectral
 
 
 class Fourier(Spectral):
-    """Fourier polynomials.
+    r"""Fourier polynomials.
     
     Parameters
     ----------
     order:
-        The maximum order of the polynomials.
+        The number of sine functions the basis is composed of. The 
+        total number of basis functions, $N$, is equal to `2*order+2`.
+    
+    Notes
+    -----
+    The Fourier basis for the interval $[-1, 1]$, with cardinality $N$, 
+    is given by
+    $$
+        \left\{\frac{1}{2}, \sqrt{2}\sin(\pi x), \dots, \sqrt{2}\sin(n \pi x), 
+        \sqrt{2}\cos(\pi x), \dots, \sqrt{2}\cos(n \pi x), 
+        \sqrt{2}\cos(N \pi x / 2)\right\},
+    $$
+    where $n = 1, 2, \dots, \tfrac{N}{2}-1$. 
+    
+    The basis functions are orthonormal with respect to the 
+    (normalised) weight function given by
+    $$\lambda(x) = \frac{1}{2}.$$
+
+    A given function can be represented in the Fourier basis as 
+    $$
+        f(x) \approx 
+            a_{0} 
+            + \sum_{n=1}^{N/2}a_{n} \sqrt{2} \cos(n \pi x) 
+            + \sum_{n=1}^{N/2-1}b_{n} \sqrt{2} \sin(n \pi x),
+    $$
+    where the coefficients can be computed using the trapezoidal rule:
+    $$
+        \begin{align}
+            a_{0} &= \frac{1}{N}\sum_{k=1}^{N} f(x_{k}), && \\
+            a_{n} &= \frac{1}{N}\sum_{k=1}^{N} f(x_{k}) \sqrt{2} \cos(n \pi x_{k}), 
+                &&\quad n = 1, 2, \dots, \frac{N}{2} - 1, \\
+            b_{n} &= \frac{1}{N}\sum_{k=1}^{N} f(x_{k}) \sqrt{2} \sin(n \pi x_{k}),
+                &&\quad n = 1, 2, \dots, \frac{N}{2} - 1, \\
+            a_{N/2} &= \frac{1}{2N}\sum_{k=1}^{N} f(x_{k}) \sqrt{2} \cos(N \pi x_{k} / 2).
+                &&
+        \end{align}
+    $$
+    The collocation points $\{x_{n}\}_{n=1}^{N}$ are given by
+    $$
+        x_{n} = -1 + \frac{2n}{N}.
+    $$
+
+    References
+    ----------
+    Boyd, JP (2001, Section 4.5). *[Chebyshev and Fourier spectral 
+    methods](https://link.springer.com/book/9783540514879).* Lecture 
+    Notes in Engineering, Volume 49.
+
+    Cui, T and Dolgov, S (2022). *[Deep composition of Tensor-Trains 
+    using squared inverse Rosenblatt transports](https://doi.org/10.1007/s10208-021-09537-5).* 
+    Foundations of Computational Mathematics, **22**, 1863--1922.
         
     """
 
@@ -22,7 +72,7 @@ class Fourier(Spectral):
         self.order = order
         self.m = order + 1
         self.c = torch.pi * (torch.arange(order) + 1)
-        self.nodes = (n+1) / (0.5*n_nodes) - 1
+        self.nodes = 2.0 * (n+1) / n_nodes - 1
         self.weights = torch.ones_like(self.nodes) / n_nodes
 
         self.__post_init__()
