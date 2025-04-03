27cc4f3023af5eb40e60825d738d87adf050d792
alexgdebeer <alexgdebeer@gmail.com>
2025-04-03 15:58:18 +1100

    Add prior transformation class

---
+++ deep_tensor/prior_transformation.py
@@ -0,0 +1,37 @@
+from typing import Callable
+
+from torch import Tensor
+
+from .references import Reference
+
+
+class PriorTransformation():
+    """A mapping between the prior and a reference random variable.
+    
+    TODO: eventually, this will need to be able to evaluate the 
+    action of the Jacobian of the Q^{-1}.
+
+    TODO: perhaps this could be a dataclass.
+    
+    Parameters
+    ----------
+    reference:
+        The density of the reference random variable.
+    Q:
+        A function which maps from the prior to the reference 
+        distribution.
+    Q_inv: 
+        The inverse of `Q`.
+
+    """
+
+    def __init__(
+        self, 
+        reference: Reference,
+        Q: Callable[[Tensor], Tensor],
+        Q_inv: Callable[[Tensor], Tensor]
+    ):
+        self.reference = reference
+        self.Q = Q
+        self.Q_inv = Q_inv
+        return
