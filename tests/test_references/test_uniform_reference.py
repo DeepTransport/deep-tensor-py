400c6ad9daea4076576f2a4a236d10aecaf31471
alexgdebeer <alexgdebeer@gmail.com>
2025-05-09 10:57:20 +1000

    Fix tests for UniformReference

--- tests/test_references/test_uniform_reference.py
+++ tests/test_references/test_uniform_reference.py
@@ -11,9 +11,7 @@ torch.manual_seed(0)
 class TestUniformReference(unittest.TestCase):
 
     def build_reference(self) -> dt.UniformReference:
-        bounds = torch.tensor([1.0, 3.0])
-        domain = dt.BoundedDomain(bounds)
-        ref = dt.UniformReference(domain)
+        ref = dt.UniformReference()
         return ref
 
     def test_pdf_and_cdf(self):
@@ -23,14 +21,14 @@ class TestUniformReference(unittest.TestCase):
 
         ref = self.build_reference()
 
-        rs = torch.tensor([1., 1.5, 2., 2.5, 3.])
+        rs = torch.tensor([0., 0.25, 0.5, 0.75, 1.0])
 
         zs, dzdrs = ref.eval_cdf(rs)
         zs_true = torch.tensor([0., 0.25, 0.5, 0.75, 1.0])
-        dzdrs_true = torch.tensor([0.5, 0.5, 0.5, 0.5, 0.5])
+        dzdrs_true = torch.ones(5)
 
         pdfs, grad_pdfs = ref.eval_pdf(rs)
-        pdfs_true = torch.tensor([0.5, 0.5, 0.5, 0.5, 0.5])
+        pdfs_true = torch.ones(5)
         grad_pdfs_true = torch.zeros(5)
 
         self.assertTrue((zs - zs_true).abs().max() < 1e-8)
@@ -45,24 +43,24 @@ class TestUniformReference(unittest.TestCase):
 
         ref = self.build_reference()
 
-        zs = torch.tensor([0., 0.25, 0.5, 0.75, 1.])
+        zs = torch.tensor([0.0, 0.25, 0.5, 0.75, 1.0])
         rs = ref.invert_cdf(zs)
-        rs_true = torch.tensor([1., 1.5, 2., 2.5, 3.])
+        rs_true = torch.tensor([0.0, 0.25, 0.5, 0.75, 1.0])
 
         self.assertTrue((rs - rs_true).abs().max() < 1e-8)
         return
     
-    def test_log_joint_pdf(self):
-        """Tests the the log_joint_pdf method works correctly.
+    def test_eval_potential(self):
+        """Tests that the eval_potential method works correctly.
         """
 
         ref = self.build_reference()
 
-        rs = 1. + 2. * torch.rand(3, 4)
+        rs = torch.rand(3, 4)
 
-        log_pdfs, log_grad_pdfs = ref.log_joint_pdf(rs)
+        log_pdfs, log_grad_pdfs = ref.eval_potential(rs)
 
-        log_pdfs_true = torch.full((3,), torch.tensor(1./16.).log())
+        log_pdfs_true = torch.full((3,), torch.tensor(1.0).log())
         log_grad_pdfs_true = torch.zeros(3)
 
         self.assertTrue((log_pdfs - log_pdfs_true).abs().max() < 1e-8)
@@ -70,20 +68,19 @@ class TestUniformReference(unittest.TestCase):
         return
     
     def test_sample(self):
-        """Tests that the sampling methods work as intended.
-        """
+        """Tests that the sampling methods work as intended."""
 
         ref = self.build_reference()
 
         rs = ref.random(d=3, n=4)
         self.assertEqual(rs.shape, torch.Size([4, 3]))
-        self.assertTrue(rs.min() >= 1.)
-        self.assertTrue(rs.max() <= 3.)
+        self.assertTrue(rs.min() >= 0.0)
+        self.assertTrue(rs.max() <= 1.0)
 
         rs = ref.sobol(d=3, n=4)
         self.assertEqual(rs.shape, torch.Size([4, 3]))
-        self.assertTrue(rs.min() >= 1.)
-        self.assertTrue(rs.max() <= 3.)
+        self.assertTrue(rs.min() >= 0.0)
+        self.assertTrue(rs.max() <= 1.0)
         return
 
 
