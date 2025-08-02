1cf54a5571fcae783550f1e95e272d187517c59d
alexgdebeer <alexgdebeer@gmail.com>
2025-08-02 15:18:06 +1000

    Add tests for GaussianReference

---
+++ tests/test_references/test_gaussian_reference.py
@@ -0,0 +1,57 @@
+import math
+import unittest 
+
+import torch
+
+import deep_tensor as dt
+
+
+
+class TestGaussianReference(unittest.TestCase):
+
+    def test_pdf_and_cdf(self):
+
+        ref = dt.GaussianReference()
+
+        # Compute area under density
+        cdf_left = 0.5 * (1.0 - math.erf(2.0 * math.sqrt(2.0)))
+        norm = 1.0 - 2.0 * cdf_left
+
+        rs = torch.tensor([-4.0, -2.0, 0.0, 2.0, 4.0])
+
+        pdfs, grad_pdfs = ref.eval_pdf(rs)  # TODO: check the gradient
+        cdfs, grad_cdfs = ref.eval_cdf(rs)
+
+        # Compute exact PDF
+        exps = torch.tensor([math.exp(8), math.exp(2), 1.0, math.exp(2), math.exp(8)])
+        pdfs_true = (1.0 / (exps * math.sqrt(2.0 * math.pi))) / norm
+
+        # Compute exact CDF
+        cdfs_true = torch.tensor([0.5 * (1 - math.erf(2.0 * math.sqrt(2))), 
+                                  0.5 * (1 - math.erf(math.sqrt(2))), 
+                                  0.5, 
+                                  0.5 * (math.erf(math.sqrt(2)) + 1), 
+                                  0.5 * (math.erf(2*math.sqrt(2)) + 1)])
+        cdfs_true = (cdfs_true - cdf_left) / norm
+        
+        self.assertTrue((pdfs-pdfs_true).abs().max() < 1e-6)
+        self.assertTrue((grad_cdfs-pdfs_true).abs().max() < 1e-6)
+
+        self.assertTrue((cdfs-cdfs_true).abs().max() < 1e-6)
+        return 
+    
+    def test_sample(self):
+        """Tests that the sampling methods work as intended."""
+
+        ref = dt.GaussianReference()
+
+        rs = ref.random(d=3, n=4)
+        self.assertEqual(rs.shape, torch.Size([4, 3]))
+        self.assertTrue(rs.min() >= -4.0)
+        self.assertTrue(rs.max() <= 4.0)
+
+        rs = ref.sobol(d=3, n=4)
+        self.assertEqual(rs.shape, torch.Size([4, 3]))
+        self.assertTrue(rs.min() >= -4.0)
+        self.assertTrue(rs.max() <= 4.0)
+        return
--- tests/test_references/test_uniform_reference.py
+++ tests/test_references/test_uniform_reference.py
@@ -8,69 +8,68 @@ import deep_tensor as dt
 torch.manual_seed(0)
 
 
-class TestUniformReference(unittest.TestCase):
+EPS = 1e-6
 
-    def build_reference(self) -> dt.UniformReference:
-        ref = dt.UniformReference()
-        return ref
+
+class TestUniformReference(unittest.TestCase):
 
     def test_pdf_and_cdf(self):
         """Tests that the PDF and CDF of the uniform reference 
         distribution are evaluated correctly.
         """
 
-        ref = self.build_reference()
+        ref = dt.UniformReference()
 
-        rs = torch.tensor([0., 0.25, 0.5, 0.75, 1.0])
+        rs = torch.tensor([0.0, 0.25, 0.5, 0.75, 1.0])
 
         zs, dzdrs = ref.eval_cdf(rs)
-        zs_true = torch.tensor([0., 0.25, 0.5, 0.75, 1.0])
+        zs_true = torch.tensor([0.0, 0.25, 0.5, 0.75, 1.0])
         dzdrs_true = torch.ones(5)
 
         pdfs, grad_pdfs = ref.eval_pdf(rs)
         pdfs_true = torch.ones(5)
         grad_pdfs_true = torch.zeros(5)
 
-        self.assertTrue((zs - zs_true).abs().max() < 1e-8)
-        self.assertTrue((dzdrs - dzdrs_true).abs().max() < 1e-8)
-        self.assertTrue((pdfs - pdfs_true).abs().max() < 1e-8)
-        self.assertTrue((grad_pdfs - grad_pdfs_true).abs().max() < 1e-8)
+        self.assertTrue((zs - zs_true).abs().max() < EPS)
+        self.assertTrue((dzdrs - dzdrs_true).abs().max() < EPS)
+        self.assertTrue((pdfs - pdfs_true).abs().max() < EPS)
+        self.assertTrue((grad_pdfs - grad_pdfs_true).abs().max() < EPS)
         return
 
     def test_inverse_cdf(self):
         """Tests that the invert_cdf method works correctly.
         """
 
-        ref = self.build_reference()
+        ref = dt.UniformReference()
 
         zs = torch.tensor([0.0, 0.25, 0.5, 0.75, 1.0])
         rs = ref.invert_cdf(zs)
+        
         rs_true = torch.tensor([0.0, 0.25, 0.5, 0.75, 1.0])
 
-        self.assertTrue((rs - rs_true).abs().max() < 1e-8)
+        self.assertTrue((rs - rs_true).abs().max() < EPS)
         return
     
     def test_eval_potential(self):
         """Tests that the eval_potential method works correctly.
         """
 
-        ref = self.build_reference()
+        ref = dt.UniformReference()
 
         rs = torch.rand(3, 4)
-
         log_pdfs, log_grad_pdfs = ref.eval_potential(rs)
 
-        log_pdfs_true = torch.full((3,), torch.tensor(1.0).log())
+        log_pdfs_true = torch.zeros(3)
         log_grad_pdfs_true = torch.zeros(3)
 
-        self.assertTrue((log_pdfs - log_pdfs_true).abs().max() < 1e-8)
-        self.assertTrue((log_grad_pdfs - log_grad_pdfs_true).abs().max() < 1e-8)
+        self.assertTrue((log_pdfs - log_pdfs_true).abs().max() < EPS)
+        self.assertTrue((log_grad_pdfs - log_grad_pdfs_true).abs().max() < EPS)
         return
     
     def test_sample(self):
         """Tests that the sampling methods work as intended."""
 
-        ref = self.build_reference()
+        ref = dt.UniformReference()
 
         rs = ref.random(d=3, n=4)
         self.assertEqual(rs.shape, torch.Size([4, 3]))
