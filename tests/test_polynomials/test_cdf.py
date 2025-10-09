09e5152402516c125e98de90b3c736c8373053ad
alexgdebeer <alexgdebeer@gmail.com>
2025-10-09 17:40:27 -0400

    Add tighter CDF inversion tolerance

--- tests/test_polynomials/test_cdf.py
+++ tests/test_polynomials/test_cdf.py
@@ -44,7 +44,7 @@ class TestCDF(unittest.TestCase):
                 zs = cdf.eval_cdf(ps, ls)
                 ls_0 = cdf.invert_cdf(ps, zs)
                 
-                self.assertTrue((ls-ls_0).abs().max() < 1e-4)
+                self.assertTrue((ls-ls_0).abs().max() < 1e-10)
 
         return
 
