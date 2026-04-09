74d90bfa4769fc76959b193637060984c1b52ff7
alexgdebeer <alexgdebeer@gmail.com>
2026-04-09 15:40:05 -0500

    Add tests for AffineMapping

--- deep_tensor/preconditioners/affine_mapping.py
+++ deep_tensor/preconditioners/affine_mapping.py
@@ -9,23 +9,18 @@ from ..references import GaussianReference
 
 
 class AffineMapping(Preconditioner):
-    r"""A mapping between two Gaussian densities.
-    
-    This preconditioner provides a mapping between the standard 
-    Gaussian density and an Gaussian density with an arbitrary mean and 
-    covariance.
+    r"""An affine transformation.
 
     Parameters
     ----------
-    mean:
-        The mean of the target Gaussian density.
-    cov:
-        The covariance matrix of the target Gaussian density.
+    A:
+        A non-singular n*n matrix.
+    b:
+        An n-dimensional vector. If not provided, this will be set to 
+        zero.
     reference:
         The reference density. If this is not specified, it will be set 
         to the unit Gaussian density with support on $[-4, 4]^{d}$.
-    diag:
-        Whether `cov` is a diagonal matrix.
 
     """
 
@@ -33,23 +28,15 @@ class AffineMapping(Preconditioner):
         self,
         A: Tensor,
         b: Tensor | None = None, 
-        reference: GaussianReference | None = None,
-        diag: bool = False
+        reference: GaussianReference | None = None
     ):
-        
         if b is None:
             b = torch.zeros((A.shape[0],))
-
         if reference is None:
             reference = GaussianReference()
-        elif not isinstance(reference, GaussianReference):
-            msg = "Reference density must be Gaussian."
-            raise Exception(msg)
-
         self.A = A
-        self.b = b
+        self.b = b.flatten()
         self.reference = reference
-        self.diag = diag
         self.A_inv: Tensor = linalg.inv(self.A)
         self.dim = self.b.flatten().numel()
         return
