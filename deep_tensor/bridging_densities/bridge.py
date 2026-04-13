Fix bug in conversion between gradients with respect to x and u, and tidy up subspace implementation

deep_tensor/bridging_densities/bridge.py
@@ -125,7 +125,7 @@ class Bridge(abc.ABC):
             the gradient of function f with respect to u.
         
         """
-        dfdus = torch.einsum("...i, i...j", dfdxs, dxdus)
+        dfdus = torch.einsum("i...j, ...j", dxdus, dfdxs)
         return dfdus
     
     def _eval_pullback(self, us: Tensor) -> Tensor:

deep_tensor/irt/dirt.py
@@ -149,42 +149,120 @@ class DIRT():
     # def error_news(self) -> List:
     #     return [self.subspaces[k].error_new for k in range(self.num_layers)]  # type: ignore
   
-    def eval_ratio_func(self, rs: Tensor) -> Tensor:
+    def _grad_neglogbridge(self, rs: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
+        """Evaluates the gradient of the negative logarithm of the 
+        current bridging density with respect to the reference random 
+        variable.
+
+        Parameters
+        ----------
+        rs:
+            An n * d matrix containing a set of samples from the domain 
+            of the reference density.
+        
+        Returns
+        -------
+        neglogfus:
+            An n-dimensional vector containing the pushforward of the 
+            reference density under the current IRT mapping, 
+            evaluated at each sample in `rs`.
+        neglogbridges:
+            An n-dimensional vector containing the current bridging 
+            density evaluated at each sample in `rs` after the current 
+            IRT mapping is applied.
+        grad_neglogbridges:
+            An n * d matrix containing the gradient of the negative 
+            logarithm of the composition of the current IRT mapping and 
+            the current bridging density, evaluated at each sample in 
+            rs.
+
+        """
+        
+        us, neglogfus = self._eval_irt_reference(rs)
+        neglogbs, dneglogbs_dus = self.bridge.eval_gradneglog(us)
+        
+        # TODO: make this its own function -- _eval_irt_jac_reference().
+        num_rs, dim_rs = rs.shape
+        def _eval_irt(rs: Tensor) -> Tensor:
+            rs = rs.reshape(num_rs, dim_rs)
+            return self._eval_irt_reference(rs)[0].sum(dim=0)
+        dudrs: Tensor = jacobian(_eval_irt, rs.flatten(), vectorize=True)
+        dudrs = dudrs.reshape(dim_rs, num_rs, dim_rs)
+
+        dneglogbs_drs = torch.einsum("i...j, ...j", dudrs, dneglogbs_dus)
+        return neglogfus, neglogbs, dneglogbs_drs
+
+    def _eval_neglogratio(self, rs: Tensor) -> Tensor:
         """Evaluates the current ratio function at each element in rs, 
         where rs is a set of samples from the reference domain.
+
+        Parameters
+        ----------
+        rs:
+            An n * d matrix containing a set of samples in the 
+            reference domain.
+        
+        Returns
+        -------
+        neglogratios:
+            An n-dimensional vector containing the composition of the 
+            current IRT mapping and the ratio function evaluated at 
+            each sample in rs.
+        
         """
         us, neglogfus_dirt = self._eval_irt_reference(rs)
-        neglogratios = self.bridge.ratio_func(self.ratio_type, 
-                                              rs, us, neglogfus_dirt)
+        neglogratios = self.bridge.ratio_func(
+            self.ratio_type, 
+            rs, us, 
+            neglogfus_dirt
+        )
         return neglogratios
+    
+    def _grad_neglogratio(self, rs: Tensor):
+        """TODO: write this. should make use of a new function, 
+        self.bridge.grad_neglogratio(), which I will write at some point...
+        
+        this will be useful for when the basis is replaced at each 
+        iteration..
+        """
+        raise NotImplementedError()
+        return
+    
+    def _eval_neglogprofile(self, rs: Tensor) -> Tensor:
+        """Evaluates the negative logarithm of the profile function.
+        
+        Parameters
+        ----------
+        rs:
+            An n * d matrix containing a set of samples from the 
+            reference domain.
+        
+        Returns
+        -------
+        neglogprofiles:
+            An n-dimensional vector containing the negative logarithm 
+            of the profile function evaluated at each sample in rs.
+            
+        """
+        subspace = self.subspaces[self.num_layers]
+        neglogprofiles = subspace.eval_neglogprofile(self._eval_neglogratio, rs)
+        return neglogprofiles
 
     def _get_new_layer(self) -> None:
         """Constructs a new SIRT to add to the current composition of 
         SIRTs.
         """
         k = self.num_layers
-        # If the subspace has changed, it is nontrivial to use 
-        # information from the previous FTT
         if self.subspace.is_fixed and k > 0:
             ftt = self.sirts[k-1].ftt.clone()
         else:
+            # If the subspace has changed, it is nontrivial to use 
+            # information from the previous FTT
             ftt = self.ftt.clone() 
-        # TODO: compute the diagnostics at the same time as updating 
-        # the subspace..?
         self.subspaces[k] = self.subspaces[k-1].clone()
-        self.subspaces[k].update(
-            self.bridge.eval_gradneglog,
-            self._eval_irt_reference
-        )
-        def target_func(xs):
-            # TODO: maybe increment the number of function evaluations in here.
-            # currently the number of function evaluations will not be 
-            # recorded correctly if we evaluate the profile function by 
-            # averaging over multiple samples.
-            return self.subspaces[k].eval_neglogprofile(self.eval_ratio_func, xs)
-
+        self.subspaces[k].update(self._grad_neglogbridge)
         self.sirts[k] = SIRT(
-            target_func, 
+            self._eval_neglogprofile, 
             ftt, 
             self.subspaces[k].dim_red,
             self.reference, 
@@ -192,7 +270,6 @@ class DIRT():
             self.cdf_tol,
             device=self.device
         )
-
         # TODO: tidy this up..
         # rs = self.reference.random(n=1000, d=self.dim)
         # us, neglogratios_dirt = self._eval_irt_reference_i(rs, self.num_layers, subset="first")
@@ -208,13 +285,16 @@ class DIRT():
         neglogfus_dirt: Tensor | None,
         cum_time: float
     ) -> None:
-
         msg = [
             f"Iter: {self.num_layers+1:=2}",
             f"Cum. Fevals: {self.num_eval:=.2e}",
             f"Cum. Time: {cum_time:=.2e} s"
         ]
-        msg += self.bridge._get_diagnostics(log_weights, neglogfus, neglogfus_dirt)
+        msg += self.bridge._get_diagnostics(
+            log_weights, 
+            neglogfus, 
+            neglogfus_dirt
+        )
         dirt_info(" | ".join(msg))
         return
     

deep_tensor/subspaces.py
@@ -1,4 +1,5 @@
 import abc
+import logging
 import math
 from typing import Callable, Tuple
 
@@ -9,7 +10,11 @@ from .tools.printing import lis_info
 from .debiasing.importance_sampling import estimate_ess_ratio
 
 
-UPDATE_METHODS_LIS = ("augment", "rebuild", "static")
+logger = logging.getLogger(__name__)
+
+
+TARGET_FUNCS_LIS = ("bridge", "ratio")
+UPDATE_METHODS_LIS = ("augment", "rebuild", "fixed")
 
 
 class Subspace(abc.ABC):
@@ -135,31 +140,75 @@ class Subspace(abc.ABC):
     @abc.abstractmethod 
     def eval_neglogprofile(
         self, 
-        target_func: Callable[[Tensor], Tensor],
+        eval_neglogtarget: Callable[[Tensor], Tensor],
         vs_red: Tensor
     ) -> Tensor:
-        """Evalutes the negative logarithm of the profile function at a 
+        r"""Evalutes the negative logarithm of the profile function at a 
         set of points in the reduced subspace.
         
-        TODO: finish this..
+        Parameters
+        ----------
+        target_func:
+            A function that accepts an $n \times d$ matrix containing a 
+            set of samples in the reference domain, and returns an 
+            $n$-dimensional vector containing the negative logarithm of 
+            the target function (composed with the current IRT mapping) 
+            evaluated at each sample.
+        vs_red:
+            An $n \times d_{r}$ matrix containing the coefficients 
+            associated with a set of samples in the reduced subspace.
+
+        Returns
+        -------
+        neglogprofiles:
+            An $n$-dimensional vector containing the negative 
+            logarithm of the profile function evaluated at each of the 
+            samples in `vs_red`.
+
         """
         pass
 
     @abc.abstractmethod 
     def update(
         self,
-        grad_neglogtarget: Callable[[Tensor], Tuple[Tensor, Tensor]],
-        irt_func: Callable[[Tensor], Tuple[Tensor, Tensor]]
+        grad_neglogbridge: Callable[[Tensor], Tuple[Tensor, Tensor, Tensor]]
     ) -> None:
+        r"""Updates the basis associated with the current reduced subspace.
+        
+        Parameters
+        ----------
+        grad_neglogtarget:
+            A function that accepts an $n \times d$ matrix containing a 
+            set of samples in the approximation domain, and return an
+            $n$-dimensional vector containing the negative logarithm of 
+            the target function evaluated at each sample, and an 
+            $n \times d$ matrix containing the gradient of the negative 
+            logarithm of the target function evaluated at each sample.
+        irt_func:
+            A function that evaluates the current inverse Rosenblatt 
+            transport.
+
+        TODO:
+        combine grad_neglogtarget and irt_func together.. and fix docstring
+        
+        """
         pass
 
     @abc.abstractmethod 
     def clone(self) -> Subspace:
+        """Returns a copy of the subspace."""
         pass
 
 
 class IdentitySubspace(Subspace):
-    """Identity subspace."""
+    r"""Identity subspace (*i.e.,* the DIRT will be constructed on the full space).
+    
+    Parameters
+    ----------
+    dim:
+        The dimension of the target density.
+    
+    """
 
     def __init__(self, dim: int):
         self.basis_red = torch.eye(dim)
@@ -174,15 +223,14 @@ class IdentitySubspace(Subspace):
     
     def eval_neglogprofile(
         self, 
-        target_func: Callable[[Tensor], Tensor], 
+        eval_neglogtarget: Callable[[Tensor], Tensor], 
         xs: Tensor
     ) -> Tensor:
-        return target_func(xs)
+        return eval_neglogtarget(xs)
     
     def update(
         self, 
-        grad_neglogtarget: Callable[[Tensor], Tuple[Tensor, Tensor]],
-        irt_func: Callable[[Tensor], Tuple[Tensor, Tensor]]
+        grad_neglogbridge: Callable[[Tensor], Tuple[Tensor, Tensor, Tensor]]
     ) -> None: 
         return
 
@@ -228,19 +276,24 @@ class LikelihoodInformedSubspace(Subspace):
         initial_basis: Tensor | None = None
     ):
 
-        # TODO: check target_func and update_method are valid.
+        target_func = target_func.lower()
         update_method = update_method.lower()
-        if update_method not in UPDATE_METHODS_LIS:
+        if target_func not in TARGET_FUNCS_LIS:
             msg = (
-                "Unknown update method. Accepted methods are "
-                + f"{", ".join(UPDATE_METHODS_LIS)}."
+                "Unknown target function. Accepted target functions are `"
+                f"{"`, `".join(TARGET_FUNCS_LIS)}`."
             )
             raise Exception(msg)
-
-        if update_method == "static" and initial_basis is None:
+        if update_method not in UPDATE_METHODS_LIS:
             msg = (
-                "If update_method==`static`, need to pass in "
-                "an initial basis."
+                "Unknown update method. Accepted methods are `"
+                f"{"`, `".join(UPDATE_METHODS_LIS)}`."
+            )
+            raise Exception(msg)
+        if update_method == "fixed" and initial_basis is None:
+            msg = (
+                "If update_method==`fixed`, an initial basis must be "
+                "supplied."
             )
             raise Exception(msg)
 
@@ -252,18 +305,19 @@ class LikelihoodInformedSubspace(Subspace):
         self.eps = eps
         self.num_eval = 0
         self.num_eval_grad = 0
-        
         self.initial_basis = initial_basis
+        self.max_comp = dim - 2  # subspace must be at least two-dimensional
 
         if self.initial_basis is None:
             self.basis_red = torch.zeros((dim, 0))
             self.basis_comp = torch.eye(dim)
-
         if self.initial_basis is not None:
-            self.basis_red = self.initial_basis
+            self.basis_red = self.initial_basis.clone()
             self.basis_comp = self._compute_basis_comp(self.basis_red)
             if self.fixed_comp and self.num_comp > 0:
-                self.vs_comp = torch.randn((self.num_comp, self.dim_comp))
+                # TODO: fix the device. probably pass in self.device as an attribute
+                shape_comp = (self.num_comp, self.dim_comp)
+                self.vs_comp = torch.randn(shape_comp)
                 self.xs_comp = self.eval_coef2comp(self.vs_comp)
 
         self.P_red = self.basis_red @ self.basis_red.T
@@ -277,11 +331,8 @@ class LikelihoodInformedSubspace(Subspace):
     
     def update(
         self, 
-        grad_neglogtarget: Callable[[Tensor], Tuple[Tensor, Tensor]],
-        irt_func: Callable[[Tensor], Tuple[Tensor, Tensor]]
+        grad_neglogbridge: Callable[[Tensor], Tuple[Tensor, Tensor, Tensor]]
     ) -> None:
-        """test...
-        """
 
         if self.update_method == "static":
             return
@@ -293,25 +344,28 @@ class LikelihoodInformedSubspace(Subspace):
         lis_info("Computing estimate of Gram matrix...", end="\r")
 
         # Generate a set of samples distributed according to biasing 
-        # density
+        # density. TODO: should these be generated according to reference?
+        # TODO: fix device.
         rs = torch.randn((self.num_samples_gram, self.dim))
-        us, neglogfus = irt_func(rs)
-        neglogbridges, grad_neglogbridges = grad_neglogtarget(us)
+        neglogfus, neglogbridges, grad_neglogbridges = grad_neglogbridge(rs)
 
-        self.num_eval += us.shape[0]
-        self.num_eval_grad += us.shape[0]
+        self.num_eval += rs.shape[0]
+        self.num_eval_grad += rs.shape[0]
 
         log_weights = neglogfus - neglogbridges
         log_weights -= log_weights.max()
         weights = log_weights.exp() / log_weights.exp().sum()
         ess = estimate_ess_ratio(log_weights) * weights.numel()
 
-        if weights.isnan().any(): # TODO: should also check the gradients for nans, before and after taking the reference off..
-            print("nan weights found..")
+        # TODO: should also check the gradients for nans, before and 
+        # after taking the reference off..
+        if weights.isnan().any():
+            msg = "Some weights take NaN values."
+            logger.warning(msg)
 
         # Subtract the contribution of the standard Gaussian to the 
         # gradient of the bridging density
-        grad_neglogref_us = us.clone()
+        grad_neglogref_us = rs.clone()
         grad_neglogliks = grad_neglogbridges - grad_neglogref_us
 
         H = torch.zeros((self.dim, self.dim))
@@ -323,7 +377,7 @@ class LikelihoodInformedSubspace(Subspace):
         energies = torch.cumsum(eigvals.abs(), dim=0)
 
         dim_comp = (0.5 * torch.sqrt(energies) < self.eps).sum()
-        # TODO: check that the dimension of the basis is appropriate.
+        # dim_comp = torch.clamp(dim_comp, max=self.max_comp)
 
         # Update basis
         basis_up = eigvecs[:, dim_comp:].flip(dims=(1,))
@@ -339,39 +393,40 @@ class LikelihoodInformedSubspace(Subspace):
         lis_info(" | ".join(diagnostics).ljust(40))
 
         # Estimate some errors
-        self.error_acc = torch.trace(self.P_comp @ H @ self.P_comp )
-        eigvals, _ = torch.linalg.eigh(H)
-        self.error_new = torch.sum(eigvals[:self.dim_comp])
+        # self.error_acc = torch.trace(self.P_comp @ H @ self.P_comp)
+        # eigvals, _ = torch.linalg.eigh(H)
+        # self.error_new = torch.sum(eigvals[:self.dim_comp])
         
         # Recompute fixed samples in the complement subspace
         if self.fixed_comp and self.num_comp > 0:
-            self.vs_comp = torch.randn((self.num_comp, self.dim_comp))
+            self.vs_comp = torch.randn((self.num_comp, self.dim_comp)) # TODO: fix the device..
             self.xs_comp = self.eval_coef2comp(self.vs_comp)
         
         return 
     
     def eval_neglogprofile(
         self, 
-        target_func: Callable[[Tensor], Tensor], 
+        eval_neglogratio: Callable[[Tensor], Tensor], 
         vs_red: Tensor
     ) -> Tensor:
 
         xs_red = self.eval_coef2red(vs_red)
-        num_xs_red = xs_red.shape[0]
 
         if self.num_comp == 0:
-            return target_func(xs_red)
+            return eval_neglogratio(xs_red)
         
         if self.vs_comp is None:
             # Generate a new set of samples in the complement subspace
-            self.vs_comp = torch.randn((self.num_comp, self.dim_comp))
+            shape_comp = (self.num_comp, self.dim_comp)
+            self.vs_comp = torch.randn(shape_comp, device=vs_red.device)
             self.xs_comp = self.eval_coef2comp(self.vs_comp)
+        
+        num_red = xs_red.shape[0]
         num_comp = self.xs_comp.shape[0]
-
         xs = xs_red[:, None, :] + self.xs_comp[None, :, :]
-        xs = xs.reshape(-1, self.dim_red+self.dim_comp)
-        neglogfxs = target_func(xs)
-        neglogfxs = neglogfxs.reshape(num_xs_red, num_comp)
+        xs = xs.reshape(-1, self.dim_red + self.dim_comp)
+        neglogfxs = eval_neglogratio(xs)
+        neglogfxs = neglogfxs.reshape(num_red, num_comp)
         neglogfxs_mean = (
             - torch.logsumexp(-neglogfxs, dim=1)
             + math.log(num_comp)