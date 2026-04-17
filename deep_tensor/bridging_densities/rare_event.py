Fix issues with numerical stability

deep_tensor/bridging_densities/rare_event.py
@@ -360,6 +360,7 @@ class SmoothedIndicator(Bridge, abc.ABC):
             + beta * grad_neglogfus
             + grad_negloginds
         )
+        grad_neglogbridges = torch.nan_to_num(grad_neglogbridges)
         # Change variable such that gradient is w.r.t. r
         grad_neglogbridges = self._grad_chain(grad_neglogbridges, dudrs)
 
@@ -508,7 +509,7 @@ class GaussianSmoothing(SmoothedIndicator):
     
     def grad_neglogsmoothind(self, gamma: float, Fs: Tensor) -> Tuple[Tensor, Tensor]:
         lsfs = self.target_func.threshold - Fs
-        neglogpdfs = lsfs**2 * gamma**2 + 0.5*math.log(math.pi / (gamma**2))
+        neglogpdfs = lsfs**2 * gamma**2 + 0.5*torch.log(torch.pi / (torch.tensor(gamma)**2))
         neglogcdfs = self.neglogsmoothind(gamma, Fs)
         grad_neglogcdfs = -torch.exp(neglogcdfs - neglogpdfs)
         return neglogcdfs, grad_neglogcdfs