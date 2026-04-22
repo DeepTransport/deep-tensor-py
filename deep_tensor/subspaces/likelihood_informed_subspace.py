4e87d7a277e9e2c54d4f24e50c59eebe4f64a749
alexgdebeer <alexgdebeer@gmail.com>
2026-04-22 11:48:09 -0500

    Separate subspace implementations and fix issue with IdentitySubspace when evaluating marginals

--- deep_tensor/subspaces/likelihood_informed_subspace.py
+++ deep_tensor/subspaces/likelihood_informed_subspace.py
@@ -1,4 +1,3 @@
-import abc
 import logging
 import math
 from typing import Callable, Tuple
@@ -7,239 +6,15 @@ import warnings
 import torch 
 from torch import Tensor
 
-from .tools.printing import lis_info
-from .debiasing.importance_sampling import estimate_ess_ratio
+from .subspace import Subspace
+from ..debiasing.importance_sampling import estimate_ess_ratio
+from ..tools.printing import lis_info
 
 
 logger = logging.getLogger(__name__)
 
-
 TARGET_FUNCS_LIS = ("bridge", "ratio")
 UPDATE_METHODS_LIS = ("augment", "rebuild", "fixed")
-
-
-class Subspace(abc.ABC):
-
-    @property 
-    @abc.abstractmethod 
-    def is_fixed(self) -> bool:
-        pass
-    
-    @property 
-    def basis_red(self) -> Tensor:
-        return self._basis_red 
-    
-    @basis_red.setter
-    def basis_red(self, val: Tensor) -> None:
-        self._basis_red = val 
-        return
-    
-    @property 
-    def basis_comp(self) -> Tensor:
-        return self._basis_comp
-    
-    @basis_comp.setter
-    def basis_comp(self, val: Tensor) -> None:
-        self._basis_comp = val 
-        return
-    
-    @property 
-    def num_eval(self) -> int:
-        return self._num_eval
-    
-    @num_eval.setter
-    def num_eval(self, val: int) -> None:
-        self._num_eval = val 
-        return
-    
-    @property 
-    def num_eval_grad(self) -> int:
-        return self._num_eval_grad
-    
-    @num_eval_grad.setter
-    def num_eval_grad(self, val: int) -> None:
-        self._num_eval_grad = val 
-        return
-    
-    @property 
-    def dim(self) -> int:
-        return self.dim_red + self.dim_comp
-
-    @property
-    def dim_red(self) -> int:
-        return self.basis_red.shape[1]
-
-    @property 
-    def dim_comp(self) -> int:
-        return self.basis_comp.shape[1]
-    
-    @property 
-    def P_red(self) -> Tensor:
-        return self._P_red
-    
-    @P_red.setter
-    def P_red(self, val: Tensor) -> None:
-        self._P_red = val 
-        return
-    
-    @property 
-    def P_comp(self) -> Tensor:
-        return self._P_comp
-    
-    @P_comp.setter
-    def P_comp(self, val: Tensor) -> None:
-        self._P_comp = val 
-        return
-
-    def eval_coef2red(self, vs: Tensor) -> Tensor:
-        """Computes the reduced subspace vectors associated with a 
-        set of coefficients.
-        """
-        vs = torch.atleast_2d(vs)
-        return vs @ self.basis_red.T 
-    
-    def eval_red2coef(self, xs: Tensor) -> Tensor:
-        """Computes the reduced subspace coefficients associated with a 
-        set of vectors.
-        """
-        xs = torch.atleast_2d(xs)
-        return xs @ self.basis_red
-    
-    def eval_coef2comp(self, ws: Tensor) -> Tensor:
-        """Computes the complement subspace vectors associated with a 
-        set of coefficients.
-        """
-        ws = torch.atleast_2d(ws)
-        return ws @ self.basis_comp.T
-    
-    def eval_comp2coef(self, xs: Tensor) -> Tensor:
-        """Computes the complement subspace coefficients associated 
-        with a set of vectors.
-        """
-        xs = torch.atleast_2d(xs)
-        return xs @ self.basis_comp
-    
-    def project_red(self, xs: Tensor) -> Tensor:
-        """Projects a set of vectors onto the LDT subspace."""
-        xs = torch.atleast_2d(xs)
-        return xs @ self.P_red
-    
-    def project_comp(self, xs: Tensor) -> Tensor:
-        """Projects a set of vectors onto the complement subspace."""
-        xs = torch.atleast_2d(xs)
-        return xs @ self.P_comp
-    
-    def _compute_basis_comp(self, basis_red: Tensor) -> Tensor:
-        """Given a basis for the reduced subspace, computes a basis for 
-        the complement subspace.
-        """
-        P_comp = torch.eye(basis_red.shape[0]) - basis_red @ basis_red.T
-        _, eigvecs = torch.linalg.eigh(P_comp)
-        basis_comp = eigvecs[:, self.dim_red:]
-        return basis_comp
-
-    @abc.abstractmethod 
-    def eval_neglogprofile(
-        self, 
-        eval_neglogtarget: Callable[[Tensor], Tensor],
-        vs_red: Tensor
-    ) -> Tensor:
-        r"""Evalutes the negative logarithm of the profile function at a 
-        set of points in the reduced subspace.
-        
-        Parameters
-        ----------
-        target_func:
-            A function that accepts an $n \times d$ matrix containing a 
-            set of samples in the reference domain, and returns an 
-            $n$-dimensional vector containing the negative logarithm of 
-            the target function (composed with the current IRT mapping) 
-            evaluated at each sample.
-        vs_red:
-            An $n \times d_{r}$ matrix containing the coefficients 
-            associated with a set of samples in the reduced subspace.
-
-        Returns
-        -------
-        neglogprofiles:
-            An $n$-dimensional vector containing the negative 
-            logarithm of the profile function evaluated at each of the 
-            samples in `vs_red`.
-
-        """
-        pass
-
-    @abc.abstractmethod 
-    def update(
-        self,
-        grad_neglogbridge: Callable[[Tensor], Tuple[Tensor, Tensor, Tensor]],
-        grad_neglogratio: Callable[[Tensor], Tuple[Tensor, Tensor, Tensor]]
-    ) -> None:
-        r"""Updates the basis associated with the current reduced subspace.
-        
-        Parameters
-        ----------
-        grad_neglogbridge:
-            A function that accepts an $n \times d$ matrix containing a 
-            set of samples in the approximation domain, and return an
-            $n$-dimensional vector containing the negative logarithm of 
-            the target function evaluated at each sample, and an 
-            $n \times d$ matrix containing the gradient of the negative 
-            logarithm of the target function evaluated at each sample.
-        grad_neglogratio:
-            TODO: write this.
-        
-        """
-        pass
-
-    @abc.abstractmethod 
-    def clone(self) -> Subspace:
-        """Returns a copy of the subspace."""
-        pass
-
-
-class IdentitySubspace(Subspace):
-    r"""Identity subspace (*i.e.,* the DIRT will be constructed on the full space).
-    
-    Parameters
-    ----------
-    dim:
-        The dimension of the target density.
-    
-    """
-
-    def __init__(
-        self, 
-        dim: int, 
-        device: torch.device = torch.get_default_device()
-    ):
-        self.device = device
-        self.basis_red = torch.eye(dim, device=self.device)
-        self.basis_comp = torch.zeros((dim, 0), device=self.device)
-        self.num_eval = 0
-        self.num_eval_grad = 0
-        return
-    
-    @property 
-    def is_fixed(self) -> bool:
-        return True
-    
-    def eval_neglogprofile(
-        self, 
-        eval_neglogtarget: Callable[[Tensor], Tensor], 
-        xs: Tensor
-    ) -> Tensor:
-        return eval_neglogtarget(xs)
-    
-    def update(
-        self, 
-        grad_neglogbridge: Callable[[Tensor], Tuple[Tensor, Tensor, Tensor]],
-        grad_neglogratio: Callable[[Tensor], Tuple[Tensor, Tensor, Tensor]]
-    ) -> None: 
-        return
-
-    def clone(self) -> IdentitySubspace:
-        return IdentitySubspace(self.dim)
 
 
 class LikelihoodInformedSubspace(Subspace):
@@ -265,6 +40,8 @@ class LikelihoodInformedSubspace(Subspace):
         dimension of subspace.
     initial_basis:
         A set of basis vectors to initialise the subspace with.
+    device:
+        The device to carry out computations on.
 
     """
 
@@ -366,8 +143,8 @@ class LikelihoodInformedSubspace(Subspace):
         """Re-computes the (fixed) set of samples in the complement 
         subspace.
         """
-        # TODO: fix device.
-        self.vs_comp = torch.randn((self.num_comp, self.dim_comp))
+        shape_vs_comp = (self.num_comp, self.dim_comp)
+        self.vs_comp = torch.randn(shape_vs_comp, device=self.device)
         self.xs_comp = self.eval_coef2comp(self.vs_comp)
         return
     
@@ -386,7 +163,8 @@ class LikelihoodInformedSubspace(Subspace):
         
         # Generate a set of samples distributed according to biasing 
         # density. TODO: should these be generated according to reference?
-        rs = torch.randn((self.num_samples_gram, self.dim), device=self.device)
+        shape_rs = (self.num_samples_gram, self.dim)
+        rs = torch.randn(shape_rs, device=self.device)
         neglogfus, neglogbridges, grad_neglogbridges = grad_neglogbridge(rs)
 
         self.num_eval += rs.shape[0]
---
+++ deep_tensor/subspaces/subspace.py
@@ -0,0 +1,185 @@
+import abc 
+from typing import Callable, Tuple
+
+import torch
+from torch import Tensor 
+
+
+class Subspace(abc.ABC):
+
+    @property 
+    @abc.abstractmethod 
+    def is_fixed(self) -> bool:
+        pass
+    
+    @property 
+    def basis_red(self) -> Tensor:
+        return self._basis_red 
+    
+    @basis_red.setter
+    def basis_red(self, val: Tensor) -> None:
+        self._basis_red = val 
+        return
+    
+    @property 
+    def basis_comp(self) -> Tensor:
+        return self._basis_comp
+    
+    @basis_comp.setter
+    def basis_comp(self, val: Tensor) -> None:
+        self._basis_comp = val 
+        return
+    
+    @property 
+    def num_eval(self) -> int:
+        return self._num_eval
+    
+    @num_eval.setter
+    def num_eval(self, val: int) -> None:
+        self._num_eval = val 
+        return
+    
+    @property 
+    def num_eval_grad(self) -> int:
+        return self._num_eval_grad
+    
+    @num_eval_grad.setter
+    def num_eval_grad(self, val: int) -> None:
+        self._num_eval_grad = val 
+        return
+    
+    @property 
+    def dim(self) -> int:
+        return self.dim_red + self.dim_comp
+
+    @property
+    def dim_red(self) -> int:
+        return self.basis_red.shape[1]
+
+    @property 
+    def dim_comp(self) -> int:
+        return self.basis_comp.shape[1]
+    
+    @property 
+    def P_red(self) -> Tensor:
+        return self._P_red
+    
+    @P_red.setter
+    def P_red(self, val: Tensor) -> None:
+        self._P_red = val 
+        return
+    
+    @property 
+    def P_comp(self) -> Tensor:
+        return self._P_comp
+    
+    @P_comp.setter
+    def P_comp(self, val: Tensor) -> None:
+        self._P_comp = val 
+        return
+
+    def eval_coef2red(self, vs: Tensor) -> Tensor:
+        """Computes the reduced subspace vectors associated with a 
+        set of coefficients.
+        """
+        vs = torch.atleast_2d(vs)
+        return vs @ self.basis_red.T 
+    
+    def eval_red2coef(self, xs: Tensor) -> Tensor:
+        """Computes the reduced subspace coefficients associated with a 
+        set of vectors.
+        """
+        xs = torch.atleast_2d(xs)
+        return xs @ self.basis_red
+    
+    def eval_coef2comp(self, ws: Tensor) -> Tensor:
+        """Computes the complement subspace vectors associated with a 
+        set of coefficients.
+        """
+        ws = torch.atleast_2d(ws)
+        return ws @ self.basis_comp.T
+    
+    def eval_comp2coef(self, xs: Tensor) -> Tensor:
+        """Computes the complement subspace coefficients associated 
+        with a set of vectors.
+        """
+        xs = torch.atleast_2d(xs)
+        return xs @ self.basis_comp
+    
+    def project_red(self, xs: Tensor) -> Tensor:
+        """Projects a set of vectors onto the LDT subspace."""
+        xs = torch.atleast_2d(xs)
+        return xs @ self.P_red
+    
+    def project_comp(self, xs: Tensor) -> Tensor:
+        """Projects a set of vectors onto the complement subspace."""
+        xs = torch.atleast_2d(xs)
+        return xs @ self.P_comp
+    
+    def _compute_basis_comp(self, basis_red: Tensor) -> Tensor:
+        """Given a basis for the reduced subspace, computes a basis for 
+        the complement subspace.
+        """
+        P_comp = torch.eye(basis_red.shape[0]) - basis_red @ basis_red.T
+        _, eigvecs = torch.linalg.eigh(P_comp)
+        basis_comp = eigvecs[:, self.dim_red:]
+        return basis_comp
+
+    @abc.abstractmethod 
+    def eval_neglogprofile(
+        self, 
+        eval_neglogtarget: Callable[[Tensor], Tensor],
+        vs_red: Tensor
+    ) -> Tensor:
+        r"""Evalutes the negative logarithm of the profile function at a 
+        set of points in the reduced subspace.
+        
+        Parameters
+        ----------
+        target_func:
+            A function that accepts an $n \times d$ matrix containing a 
+            set of samples in the reference domain, and returns an 
+            $n$-dimensional vector containing the negative logarithm of 
+            the target function (composed with the current IRT mapping) 
+            evaluated at each sample.
+        vs_red:
+            An $n \times d_{r}$ matrix containing the coefficients 
+            associated with a set of samples in the reduced subspace.
+
+        Returns
+        -------
+        neglogprofiles:
+            An $n$-dimensional vector containing the negative 
+            logarithm of the profile function evaluated at each of the 
+            samples in `vs_red`.
+
+        """
+        pass
+
+    @abc.abstractmethod 
+    def update(
+        self,
+        grad_neglogbridge: Callable[[Tensor], Tuple[Tensor, Tensor, Tensor]],
+        grad_neglogratio: Callable[[Tensor], Tuple[Tensor, Tensor, Tensor]]
+    ) -> None:
+        r"""Updates the basis associated with the current reduced subspace.
+        
+        Parameters
+        ----------
+        grad_neglogbridge:
+            A function that accepts an $n \times d$ matrix containing a 
+            set of samples in the approximation domain, and return an
+            $n$-dimensional vector containing the negative logarithm of 
+            the target function evaluated at each sample, and an 
+            $n \times d$ matrix containing the gradient of the negative 
+            logarithm of the target function evaluated at each sample.
+        grad_neglogratio:
+            TODO: write this.
+        
+        """
+        pass
+
+    @abc.abstractmethod 
+    def clone(self) -> Subspace:
+        """Returns a copy of the subspace."""
+        pass
