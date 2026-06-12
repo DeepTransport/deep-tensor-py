05093f7579b8294dfa301e60e2b6795562d657c9
alexgdebeer <alexgdebeer@gmail.com>
2026-06-12 11:39:23 +1000

    Fix bug in chain rule computation

--- deep_tensor/bridging_densities/bridge.py
+++ deep_tensor/bridging_densities/bridge.py
@@ -205,7 +205,7 @@ class Bridge(abc.ABC):
             the gradient of function f with respect to u.
         
         """
-        dfdus = torch.einsum("i...j, ...j", dxdus, dfdxs)
+        dfdus = torch.einsum("j...i, ...j", dxdus, dfdxs)
         return dfdus
     
     def _eval_pullback(self, us: Tensor) -> Tensor:
